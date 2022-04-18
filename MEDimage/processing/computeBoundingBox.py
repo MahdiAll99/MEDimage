e4866bc80a3009311f9c2d725b01d94b29dcba7c
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-18 17:07:35 -0400

    Added documentation

--- MEDimage/processing/computeBoundingBox.py
+++ MEDimage/processing/computeBoundingBox.py
@@ -6,28 +6,16 @@ import numpy as np
 
 
 def computeBoundingBox(mask):
+    """Computes the indexes of the ROI (Region of interest) enclosing box 
+    in all dimensions.
+
+    Args:
+        mask (ndarray): ROI mask with values of 0 and 1.
+
+    Returns:
+        array: An array containing the indexes of the bounding box.
+
     """
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
-
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------    """
 
     indices = np.where(np.reshape(mask, np.size(mask), order='F') == 1)
     iV, jV, kV = np.unravel_index(indices, np.shape(mask), order='F')
