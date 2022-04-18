39a72b0c85b7a11a4991fb6c8451cc485d7b1b15
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-18 18:31:35 -0400

    Updated docstring, return types and added nested methods

--- MEDimage/processing/computeSUVmap.py
+++ MEDimage/processing/computeSUVmap.py
@@ -3,44 +3,78 @@
 
 
 import numpy as np
+from numpy import ndarray
 
 
-def computeSUVmap(rawPET, dicomH):
-    """ 
-    -------------------------------------------------------------------------
-    DESCRIPTION:
-    This function computes the SUVmap of a raw input PET volume. It is
-    assumed that the calibration factor was applied beforehand to the PET
-    volume (e.g., rawPET = rawPET*RescaleSlope + RescaleIntercept).
-    -------------------------------------------------------------------------
-    INPUTS:
-    - rawPET: 3D array representing the PET volume in raw format.
-    - dicomH: DICOM header of one of the corresponding slice of 'rawPET'.
-    -------------------------------------------------------------------------
-    OUTPUTS:
-    - SUVmap: 'rawPET' converted to SUVs (standard uptake values).
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
+def computeSUVmap(rawPET, dicomH) -> ndarray:
+    """Computes the SUVmap of a raw input PET volume. It is assumed that 
+    the calibration factor was applied beforehand to the PET volume 
+    (e.g., rawPET = rawPET*RescaleSlope + RescaleIntercept).
 
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Args:
+        rawPET (ndarray):3D array representing the PET volume in raw format.
+        dicomH (pydicom.dataset.FileDataset): DICOM header of one of the 
+            corresponding slice of 'rawPET'.
+        
+    Returns:
+        ndarray: 'rawPET' converted to SUVs (standard uptake values).
 
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------
     """
+    def dcm_hhmmss(dateStr):
+        # Converts to seconds
+        if not isinstance(dateStr, str):
+            dateStr = str(dateStr)
+        hh = float(dateStr[0:2])
+        mm = float(dateStr[2:4])
+        ss = float(dateStr[4:6])
+        totSec = hh*60.0*60.0 + mm*60.0 + ss
+        return totSec
+
+
+    def pydicom_has_tag(dcm_seq, tag):
+        # Checks if tag exists
+        return get_pydicom_meta_tag(dcm_seq, tag, test_tag=True)
+
+
+    def get_pydicom_meta_tag(dcm_seq, tag, tag_type=None, default=None,
+                            test_tag=False):
+        # Reads dicom tag
+        # Initialise with default
+        tag_value = default
+        # Read from header using simple itk
+        try:
+            tag_value = dcm_seq[tag].value
+        except KeyError:
+            if test_tag:
+                return False
+        if test_tag:
+            return True
+        # Find empty entries
+        if tag_value is not None:
+            if tag_value == "":
+                tag_value = default
+        # Cast to correct type (meta tags are usually passed as strings)
+        if tag_value is not None:
+            # String
+            if tag_type == "str":
+                tag_value = str(tag_value)
+            # Float
+            elif tag_type == "float":
+                tag_value = float(tag_value)
+            # Multiple floats
+            elif tag_type == "mult_float":
+                tag_value = [float(str_num) for str_num in tag_value]
+            # Integer
+            elif tag_type == "int":
+                tag_value = int(tag_value)
+            # Multiple floats
+            elif tag_type == "mult_int":
+                tag_value = [int(str_num) for str_num in tag_value]
+            # Boolean
+            elif tag_type == "bool":
+                tag_value = bool(tag_value)
+
+        return tag_value
 
     # Get patient weight
     if pydicom_has_tag(dcm_seq=dicomH, tag=(0x0010, 0x1030)):
@@ -79,60 +113,3 @@ def computeSUVmap(rawPET, dicomH):
     SUVmap = rawPET * weight / injected_dose_decay
 
     return SUVmap
-
-
-def dcm_hhmmss(dateStr):
-    # Converts to seconds
-    if not isinstance(dateStr, str):
-        dateStr = str(dateStr)
-    hh = float(dateStr[0:2])
-    mm = float(dateStr[2:4])
-    ss = float(dateStr[4:6])
-    totSec = hh*60.0*60.0 + mm*60.0 + ss
-    return totSec
-
-
-def pydicom_has_tag(dcm_seq, tag):
-    # Checks if tag exists
-    return get_pydicom_meta_tag(dcm_seq, tag, test_tag=True)
-
-
-def get_pydicom_meta_tag(dcm_seq, tag, tag_type=None, default=None,
-                         test_tag=False):
-    # Reads dicom tag
-    # Initialise with default
-    tag_value = default
-    # Read from header using simple itk
-    try:
-        tag_value = dcm_seq[tag].value
-    except KeyError:
-        if test_tag:
-            return False
-    if test_tag:
-        return True
-    # Find empty entries
-    if tag_value is not None:
-        if tag_value == "":
-            tag_value = default
-    # Cast to correct type (meta tags are usually passed as strings)
-    if tag_value is not None:
-        # String
-        if tag_type == "str":
-            tag_value = str(tag_value)
-        # Float
-        elif tag_type == "float":
-            tag_value = float(tag_value)
-        # Multiple floats
-        elif tag_type == "mult_float":
-            tag_value = [float(str_num) for str_num in tag_value]
-        # Integer
-        elif tag_type == "int":
-            tag_value = int(tag_value)
-        # Multiple floats
-        elif tag_type == "mult_int":
-            tag_value = [int(str_num) for str_num in tag_value]
-        # Boolean
-        elif tag_type == "bool":
-            tag_value = bool(tag_value)
-
-    return tag_value
