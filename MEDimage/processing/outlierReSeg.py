019714f7e03b6f39ea37ccf4f09d23204841e791
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-19 14:54:36 -0400

    Updated docstring and return types and removed useless argument

--- MEDimage/processing/outlierReSeg.py
+++ MEDimage/processing/outlierReSeg.py
@@ -6,41 +6,25 @@ from copy import deepcopy
 import numpy as np
 
 
-def outlierReSeg(vol, roi, outliers=None):
-    """
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
+def outlierReSeg(vol, roi) -> np.ndarray:
+    """Removes voxels with outlier intensities from the given mask
+    using the Collewet method.
 
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Args:
+        vol (ndarray): Imaging data.
+        roi (ndarray): ROI mask with values of 0 and 1.
 
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------
+    Returns:
+        array: An array with values of 0 and 1.
+    
     """
 
-    if outliers != '':
-        roi = deepcopy(roi)
+    roi = deepcopy(roi)
 
-        if outliers == "Collewet":
-            u = np.mean(vol[roi == 1])
-            sigma = np.std(vol[roi == 1])
+    u = np.mean(vol[roi == 1])
+    sigma = np.std(vol[roi == 1])
 
-            roi[vol > (u + 3*sigma)] = 0
-            roi[vol < (u - 3*sigma)] = 0
-        else:
-            raise ValueError("Outlier segmentation not defined.")
+    roi[vol > (u + 3*sigma)] = 0
+    roi[vol < (u - 3*sigma)] = 0
 
     return roi
