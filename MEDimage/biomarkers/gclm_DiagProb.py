8ae0147a70dade0da77166b4f229bcbbeab556ab
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-20 18:01:14 -0400

    Updated docstrings and return type

--- MEDimage/biomarkers/gclm_DiagProb.py
+++ MEDimage/biomarkers/gclm_DiagProb.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 import numpy as np
 
 
-def gclm_DiagProb(p_ij):
-    """Compute gclm_DiagProb.
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
+def gclm_DiagProb(p_ij) -> np.ndarray:
+    """Computes diagonal probabilities.
 
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Args:
+        p_ij (ndarray): Joint probability of grey levels 
+            i and j occurring in neighbouring voxels. (Elements
+            of the  probability distribution for grey level 
+            co-occurrences).
 
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------
+    Returns:
+        ndarray: Array of the diagonal probability.
+    
     """
 
     Ng = np.size(p_ij, 0)
