28778824bbf3636709eee298cea32e32b6ebe77a
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-20 15:36:39 -0400

    Updated docstring and return types

--- MEDimage/utils/mode.py
+++ MEDimage/utils/mode.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+from typing import Union
+
 import numpy as np
 
 
-def mode(x, return_counts=False):
-    """
-    Implementation of mode that also returns counts,
+def mode(x, return_counts=False) -> Union[np.ndarray, np.ndarray]:
+    """Implementation of mode that also returns counts,
     unlike the standard statistics.mode.
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
 
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    Args:
+        x (ndarray): n-dimensional array of which to find mode.
+        return_counts (bool): If True, also return the number 
+            of times each unique item appears in x.
+    Returns:
+        ndarray: Array of the modal (most common) value in the
+            given array.
+        ndarray: Array of the counts.
 
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------
     """
 
     unique_values, counts = np.unique(x, return_counts=True)
