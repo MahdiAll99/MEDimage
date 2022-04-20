4c2b7c0e3583abc9db7879445d7183e98efed634
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-20 14:38:29 -0400

    Moved all MEDomics statement to init file

--- MEDimage/utils/ImageVolumeObj.py
+++ MEDimage/utils/ImageVolumeObj.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
--------------------------------------------------------------------------
-AUTHOR(S): MEDomicsLab consortium
--------------------------------------------------------------------------
-STATEMENT:
-This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-a package providing MATLAB programming tools for radiomics analysis.
-    --> Copyright (C) MEDomicsLab consortium.
-
-This package is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This package is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this package.  If not, see <http://www.gnu.org/licenses/>.
--------------------------------------------------------------------------
-"""
-
 
 class ImageVolumeObj:
     """Used to organize Imaging data and their corresponding imref3d object. 
