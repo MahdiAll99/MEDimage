5ab9afc2424494630ce2a1d5a1dadac648b3304d
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-21 16:23:19 -0400

    Fixed imports issue

--- MEDimage/biomarkers/getGLRLMmatrix.py
+++ MEDimage/biomarkers/getGLRLMmatrix.py
@@ -6,10 +6,9 @@ import math
 import numpy as np
 from scipy.sparse import spdiags
 
-import biomarkers.Wei_Toolbox.rle_0 as rle
-import biomarkers.Wei_Toolbox.rle_45 as rle45
-import biomarkers.Wei_Toolbox.zigzag as zig
-
+from ..biomarkers.Wei_Toolbox.rle_0 import  rle_0 as rle
+from ..biomarkers.Wei_Toolbox.rle_45 import  rle_45 as rle45
+from ..biomarkers.Wei_Toolbox.zigzag import  zigzag as zig
 
 def getGLRLMmatrix(ROIonly, levels, distCorrection=None) -> np.ndarray:
     """Compute GLRLM matrix.
