f510ee362cfe48c8a35f722c685e6d6b077f371d
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-21 16:08:49 -0400

    Updated docstring, imports and cleaned the code

--- MEDimage/biomarkers/getGLSZMfeatures.py
+++ MEDimage/biomarkers/getGLSZMfeatures.py
@@ -1,37 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from typing import Dict
+
 import numpy as np
-import Code_Radiomics.ImageBiomarkers.getGLSZMmatrix
+
+from ..biomarkers.getGLSZMmatrix import getGLSZMmatrix
 
 
-def getGLSZMfeatures(vol):
-    """Compute GLSZMfeatures.
-    -------------------------------------------------------------------------
-    - vol: 3D volume, isotropically resampled, quantized
-      (e.g. Ng = 32, levels = [1, ..., Ng]),
-      with NaNs outside the region of interest
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
-
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------    """
+def getGLSZMfeatures(vol) -> Dict:
+    """Computes GLSZM features.
+    
+    Args:
+        vol (ndarray): 3D volume, isotropically resampled, quantized
+            (e.g. Ng = 32, levels = [1, ..., Ng]),
+            with NaNs outside the region of interest.
+    
+    Returns:
+        Dict: Dict of GLSZM features.
+        
+    """
 
     glszm = {'Fszm_sze': [],
              'Fszm_lze': [],
@@ -54,7 +42,7 @@ def getGLSZMfeatures(vol):
     # Correct definition, without any assumption
     vol = vol.copy()
     levels = np.arange(1, np.max(vol[~np.isnan(vol[:])])+1)
-    GLSZM = Code_Radiomics.ImageBiomarkers.getGLSZMmatrix.getGLSZMmatrix(vol, levels)
+    GLSZM = getGLSZMmatrix(vol, levels)
     Ns = np.sum(GLSZM)
     GLSZM = GLSZM/np.sum(GLSZM)  # Normalization of GLSZM
     sz = np.shape(GLSZM)  # Size of GLSZM
@@ -69,36 +57,29 @@ def getGLSZMfeatures(vol):
     # COMPUTING TEXTURES
 
     # Small zone emphasis
-    glszm['Fszm_sze'] = (np.matmul(pz, np.transpose(np.power(
-        1.0/np.array(cVect), 2))))
+    glszm['Fszm_sze'] = (np.matmul(pz, np.transpose(np.power(1.0/np.array(cVect), 2))))
 
     # Large zone emphasis
-    glszm['Fszm_lze'] = (np.matmul(pz, np.transpose(np.power(
-        np.array(cVect), 2))))
+    glszm['Fszm_lze'] = (np.matmul(pz, np.transpose(np.power(np.array(cVect), 2))))
 
     # Low grey level zone emphasis
     glszm['Fszm_lgze'] = np.matmul(pg, np.transpose(np.power(
         1.0/np.array(rVect), 2)))
 
     # High grey level zone emphasis
-    glszm['Fszm_hgze'] = np.matmul(pg, np.transpose(np.power(
-        np.array(rVect), 2)))
+    glszm['Fszm_hgze'] = np.matmul(pg, np.transpose(np.power(np.array(rVect), 2)))
 
     # Small zone low grey level emphasis
-    glszm['Fszm_szlge'] = np.sum(np.sum(GLSZM*(np.power(
-        1.0/rMat, 2))*(np.power(1.0/cMat, 2))))
+    glszm['Fszm_szlge'] = np.sum(np.sum(GLSZM*(np.power(1.0/rMat, 2))*(np.power(1.0/cMat, 2))))
 
     # Small zone high grey level emphasis
-    glszm['Fszm_szhge'] = np.sum(np.sum(GLSZM*(np.power(
-        rMat, 2))*(np.power(1.0/cMat, 2))))
+    glszm['Fszm_szhge'] = np.sum(np.sum(GLSZM*(np.power(rMat, 2))*(np.power(1.0/cMat, 2))))
 
     # Large zone low grey levels emphasis
-    glszm['Fszm_lzlge'] = np.sum(np.sum(GLSZM*(np.power(
-        1.0/rMat, 2))*(np.power(cMat, 2))))
+    glszm['Fszm_lzlge'] = np.sum(np.sum(GLSZM*(np.power(1.0/rMat, 2))*(np.power(cMat, 2))))
 
     # Large zone high grey level emphasis
-    glszm['Fszm_lzhge'] = np.sum(np.sum(GLSZM*(np.power(
-        rMat, 2))*(np.power(cMat, 2))))
+    glszm['Fszm_lzhge'] = np.sum(np.sum(GLSZM*(np.power(rMat, 2))*(np.power(cMat, 2))))
 
     # Gray level non-uniformity
     glszm['Fszm_glnu'] = np.sum(np.power(pg, 2)) * Ns
