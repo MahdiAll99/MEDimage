4b93c70f6cb8016009d7865ebdbe98754e22b12b
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-21 14:57:36 -0400

    Fixed issue with import and updated docstring

--- MEDimage/utils/parseContourString.py
+++ MEDimage/utils/parseContourString.py
@@ -5,10 +5,10 @@ from typing import List, Union
 
 import numpy as np
 
-from utils.strfind import strfind
+from ..utils.strfind import strfind
 
 
-def parseContourString(contourString) -> Union[float, int, np.ndarray, List[str]]:
+def parseContourString(contourString) -> Union[float, int, List[str]]:
     """Finds the delimeters('+' and '-') and the contour indexe(s)
     from the given string.
 
@@ -17,15 +17,18 @@ def parseContourString(contourString) -> Union[float, int, np.ndarray, List[str]
         delimeters. FOR EXAMPLE '3' or '1-3+2'.
 
     Returns:
-        ndarray: Array of indexes extracted from `contourString` if it's a string.
-        float: If `contourString` is float we return the passed value.
-        int: If `contourString` is int we return the passed value.
-        List[str]: List of delimeters/operations extracted from the passed value.
-    
+        Union[float, int, List[str]]: If `contourString` is a string, the return 
+            is a List of strings. If `contourString` is a an int or float we return 
+            `contourString`.
+
     Example:
         >>>contourString = '1-3+2'
         >>>parseContourString(contourString)
         [1, 2, 3], ['+', '-']
+
+        >>>contourString = 1
+        >>>parseContourString(contourString)
+        1, []
     """
 
     if isinstance(contourString, (int, float)):
@@ -39,7 +42,6 @@ def parseContourString(contourString) -> Union[float, int, np.ndarray, List[str]
     # AZ: I assume that contourNumber is an integer
     if indOperations.size == 0:
         operations = []
-
         contourNumber = [int(contourString)]
     else:
         nOp = len(indOperations)
@@ -48,8 +50,8 @@ def parseContourString(contourString) -> Union[float, int, np.ndarray, List[str]
         contourNumber = np.zeros(nOp + 1, dtype=int)
         contourNumber[0] = int(contourString[0:indOperations[0]])
         for c in np.arange(start=1, stop=nOp):
-            contourNumber[c] = int(
-                contourString[(indOperations[c-1]+1):indOperations[c]])
+            contourNumber[c] = int(contourString[(indOperations[c-1]+1) : indOperations[c]])
+
         contourNumber[-1] = int(contourString[(indOperations[-1]+1):])
         contourNumber.tolist()
 
