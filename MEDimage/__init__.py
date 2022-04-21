062f721d6a7900f8eb016c0d2b24404431af8182
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-21 14:43:58 -0400

    Added info and intialized logger

--- MEDimage/__init__.py
+++ MEDimage/__init__.py
@@ -21,3 +21,17 @@ You should have received a copy of the GNU General Public License
 along with this package.  If not, see <http://www.gnu.org/licenses/>.
 -------------------------------------------------------------------------
 """
+import logging
+
+from .utils import *
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARNING)
+logging.getLogger(__name__).addHandler(stream_handler)
+
+__author__ = "MEDomicsLab consortium"
+__version__ = "0.2.0"
+__copyright__ = "Copyright (C) MEDomicsLab consortium"
+__license__ = "GNU General Public License 3.0"
+__maintainer__ = "MAHDI AIT LHAJ LOUTFI"
+__email__ = "Mahdi.Ait.Lhaj.Loutfi@USherbrooke.ca"
