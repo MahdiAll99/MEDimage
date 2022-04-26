a7f337b915336d7ff234f92ded0c8c8c8df96e94
mahdiall99 <mehdiwinneri01@gmail.com>
2022-04-26 17:33:25 -0400

    Added new argument, updated docstring and code

--- MEDimage/processing/outlierReSeg.py
+++ MEDimage/processing/outlierReSeg.py
@@ -6,25 +6,37 @@ from copy import deepcopy
 import numpy as np
 
 
-def outlierReSeg(vol, roi) -> np.ndarray:
+def outlierReSeg(vol, roi, outliers="") -> np.ndarray:
     """Removes voxels with outlier intensities from the given mask
     using the Collewet method.
 
     Args:
         vol (ndarray): Imaging data.
         roi (ndarray): ROI mask with values of 0 and 1.
+        outliers (str, optional): Algo used to define outliers.
+            (For now this methods only implements "Collewet" method). 
 
     Returns:
         ndarray: An array with values of 0 and 1.
     
+    Raises:
+        ValueError: If `outliers` is not "Collewet" or None.
+
+    TODO:
+        * Delete outliers argument or implements others outlining methods.
+        
     """
 
-    roi = deepcopy(roi)
+    if outliers != '':
+        roi = deepcopy(roi)
 
-    u = np.mean(vol[roi == 1])
-    sigma = np.std(vol[roi == 1])
+        if outliers == "Collewet":
+            u = np.mean(vol[roi == 1])
+            sigma = np.std(vol[roi == 1])
 
-    roi[vol > (u + 3*sigma)] = 0
-    roi[vol < (u - 3*sigma)] = 0
+            roi[vol > (u + 3*sigma)] = 0
+            roi[vol < (u - 3*sigma)] = 0
+        else:
+            raise ValueError("Outlier segmentation not defined.")
 
     return roi
