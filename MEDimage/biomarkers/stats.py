9950991944f88897cca6286e99df784781ae2674
mahdiall99 <mehdiwinneri01@gmail.com>
2022-06-09 17:23:47 -0400

    renamed getStats to stats

--- MEDimage/biomarkers/stats.py
+++ MEDimage/biomarkers/stats.py
@@ -4,37 +4,29 @@
 import numpy as np
 from scipy.stats import kurtosis, skew, scoreatpercentile, iqr, variation
 
+def mean(vol):
+    """Compute statistical mean feature.
 
-def getStatsFeatures(vol, intensity=None):
+    Args:
+        vol(ndarray): 3D volume, NON-QUANTIZED, with NaNs outside the region of interest
+            --> vol: continuos imaging intensity distribution
+
+    Returns:
+        float: Statistical mean feature
+    """
+    # INITIALIZATION
+    X = vol[~np.isnan(vol[:])]
+    return np.mean(X)
+
+def extract_all(vol, intensity=None):
     """Compute StatsFeatures.
-    -------------------------------------------------------------------------
+
     - vol: 3D volume, NON-QUANTIZED, with NaNs outside the region of interest
-           --> vol: continous imaging intensity distribution
+        --> vol: continuos imaging intensity distribution
     - intensity (optional): If 'arbitrary', some feature will not be computed.
-    If 'definite', all feature will be computed. If not present as an argument,
-    all features will be computed. Here, 'filter' is the same as
-    'arbitrary'.
-    -------------------------------------------------------------------------
-    AUTHOR(S): MEDomicsLab consortium
-    -------------------------------------------------------------------------
-    STATEMENT:
-    This file is part of <https://github.com/MEDomics/MEDomicsLab/>,
-    a package providing MATLAB programming tools for radiomics analysis.
-     --> Copyright (C) MEDomicsLab consortium.
-
-    This package is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This package is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this package.  If not, see <http://www.gnu.org/licenses/>.
-    -------------------------------------------------------------------------
+        If 'definite', all feature will be computed. If not present as an argument,
+        all features will be computed. Here, 'filter' is the same as
+        'arbitrary'.
     """
 
     # PRELIMINARY
@@ -103,11 +95,10 @@ def getStatsFeatures(vol, intensity=None):
         stats['Fstat_medad'] = np.mean(np.absolute(X - np.median(X)))
         stats['Fstat_cov'] = variation(X)  # Coefficient of variation
 
-        X_75_25 = scoreatpercentile(X, 75)+scoreatpercentile(X, 25)
+        X_75_25 = scoreatpercentile(X, 75) + scoreatpercentile(X, 25)
         # Quartile coefficient of dispersion
         stats['Fstat_qcod'] = iqr(X)/X_75_25
         stats['Fstat_energy'] = np.sum(np.power(X, 2))  # Energy
-        stats['Fstat_rms'] = np.sqrt(
-            np.mean(np.power(X, 2)))  # Root mean square
+        stats['Fstat_rms'] = np.sqrt(np.mean(np.power(X, 2)))  # Root mean square
 
     return stats
