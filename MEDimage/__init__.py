7ca8f3526cb56f6a4f5aed05f832a498e068cbc2
mahdiall99 <mehdiwinneri01@gmail.com>
2022-06-27 19:55:59 -0400

    changed email and fixed imports bug

--- MEDimage/__init__.py
+++ MEDimage/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
-import MEDimage.biomarkers
-import MEDimage.processing
-import MEDimage.utils
+from . import biomarkers
+from . import processing
+from . import utils
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.WARNING)
@@ -13,4 +13,4 @@ __version__ = "0.2.0"
 __copyright__ = "Copyright (C) MEDomicsLab consortium"
 __license__ = "GNU General Public License 3.0"
 __maintainer__ = "MAHDI AIT LHAJ LOUTFI"
-__email__ = "Mahdi.Ait.Lhaj.Loutfi@USherbrooke.ca"
+__email__ = "medomics.info@gmail.com"
