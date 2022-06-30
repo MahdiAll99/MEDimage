88f098c1dc49352211eab83947c179b84b794c26
mahdiall99 <mehdiwinneri01@gmail.com>
2022-06-30 18:09:07 -0400

    Upgraded to new architecture

--- MEDimage/filter/apply_mean.py
+++ MEDimage/filter/apply_mean.py
@@ -1,27 +1,25 @@
 import numpy as np
+from MEDimage.MEDimage import MEDimage
 
 from ..MEDimageFilter import Mean
 
 
-def apply_mean(data, params):
+def apply_mean(MEDimg: MEDimage, data: np.ndarray) -> np.ndarray:
     """
-    Applies mean filter on the imaging data
+    Applies mean filter on the given data
+
+    Args:
+        MEDimg (MEDimage): Instance of the MEDimage class that holds filtering params
+        data (ndarray): Array of the imaging data
+    
+    Returns:
+        ndarray: Filtered data.
     """
     input = np.expand_dims(data.astype(np.float64), axis=0)   # Convert to shape : (B, W, H, D)
-
-    # check if it's the main params dict that passed
-    if 'imParamFilter' in params:
-        params = params['imParamFilter']
-
-    # extract Mean filter params from the passed dict    
-    if 'mean' in params:
-        params = params['mean']
-    else:
-        raise ValueError("Invalid filter parameters, mean filter params not found")
-
     _filter = Mean(
-            ndims=params['ndims'], size=params['size'], 
-            padding=params['padding'])
+            ndims=MEDimg.params.filter.mean.ndims, 
+            size=MEDimg.params.filter.mean.size, 
+            padding=MEDimg.params.filter.mean.padding)
     result = _filter.convolve(input)
 
     return np.squeeze(result)
