5d92d142c4b0d62f162abf38bbe6ebd7015c7e0f
mahdiall99 <mehdiwinneri01@gmail.com>
2022-08-02 10:53:22 -0400

    Updated package path

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,6 @@ import sphinx.builders.texinfo
 import sphinx.builders.text
 import sphinx.ext.autodoc
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('./.'))
-sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
 
