2e8e3db2ae4f02a7214cb5286685ab17c38a88c5
mahdiall99 <mehdiwinneri01@gmail.com>
2022-08-03 13:15:39 -0400

    Fixed import error

--- MEDimage/processing/__init__.py
+++ MEDimage/processing/__init__.py
@@ -1,7 +1,6 @@
 from . import *
 from .compute_suv_map import *
 from .discretisation import *
-from .get_roi_from_indexes import *
 from .interpolation import *
 from .resegmentation import *
 from .segmentation import *
