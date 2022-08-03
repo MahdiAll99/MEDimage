0ed86506ad791c85b36ac3d0deb3894e94926d3e
mahdiall99 <mehdiwinneri01@gmail.com>
2022-08-03 10:34:29 -0400

    Added new modules and moved methods aroud

--- MEDimage/processing/discretisation.py
+++ MEDimage/processing/discretisation.py
@@ -6,11 +6,64 @@ from copy import deepcopy
 from typing import Tuple
 
 import numpy as np
+from skimage.exposure import equalize_hist
 
-from ..processing.equalization import equalization
 
+def equalization(vol_re: np.ndarray) -> np.ndarray:
+    """Performs histogram equalisation of the ROI imaging intensities.
 
-def discretisation(vol_re: np.ndarray,
+    Note:
+        This is a pure "what is contained within the roi" equalization. this is
+        not influenced by the :func:`user_set_min_val()` used for FBS discretisation.
+
+    Args:
+        vol_re (ndarray): 3D array of the image volume that will be studied with
+                          NaN value for the excluded voxels (voxels outside the ROI mask).
+
+    Returns:
+        ndarray: Same input image volume but with redistributed intensities.
+    """
+
+    # AZ: This was made part of the function call
+    # n_g = 64
+    # This is the default we will use. It means that when using 'FBS',
+    # n_q should be chosen wisely such
+    # that the total number of grey levels does not exceed 64, for all
+    # patients (recommended).
+    # This choice was amde by considering that the best equalization
+    # performance for "histeq.m" is obtained with low n_g.
+    # WARNING: The effective number of grey levels coming out of "histeq.m"
+    # may be lower than n_g.
+
+    # CONSERVE THE INDICES OF THE ROI
+    x_gl = np.ravel(vol_re)
+    ind_roi = np.where(~np.isnan(vol_re))
+    x_gl = x_gl[~np.isnan(x_gl)]
+
+    # ADJUST RANGE BETWEEN 0 and 1
+    min_val = np.min(x_gl)
+    max_val = np.max(x_gl)
+    x_gl_01 = (x_gl - min_val)/(max_val - min_val)
+
+    # EQUALIZATION
+    # x_gl_equal = equalize_hist(x_gl_01, nbins=n_g)
+    # AT THE MOMENT, WE CHOOSE TO USE THE DEFAULT NUMBER OF BINS OF
+    # equalize_hist.py (256)
+    x_gl_equal = equalize_hist(x_gl_01)
+    # RE-ADJUST TO CORRECT RANGE
+    x_gl_equal = (x_gl_equal - np.min(x_gl_equal)) / \
+        (np.max(x_gl_equal) - np.min(x_gl_equal))
+    x_gl_equal = x_gl_equal * (max_val - min_val)
+    x_gl_equal = x_gl_equal + min_val
+
+    # RECONSTRUCT THE VOLUME WITH EQUALIZED VALUES
+    vol_equal_re = deepcopy(vol_re)
+
+    vol_equal_re[ind_roi] = x_gl_equal
+
+    return vol_equal_re
+
+def discretize(vol_re: np.ndarray,
                    discr_type: str,
                    n_q: float=None,
                    user_set_min_val: float=None,
--- MEDimage/processing/interpolation.py
+++ MEDimage/processing/interpolation.py
@@ -2,17 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-from copy import deepcopy
 import logging
+from copy import deepcopy
 
 import numpy as np
+from MEDimage.MEDimage import MEDimage
 
-from MEDimage import MEDimage
+from ..processing.segmentation import compute_box
 from ..utils.image_volume_obj import image_volume_obj
 from ..utils.imref import imref3d, intrinsicToWorld, worldToIntrinsic
 from ..utils.interp3 import interp3
-
-from ..processing.compute_box import compute_box
 
 _logger = logging.getLogger(__name__)
 
