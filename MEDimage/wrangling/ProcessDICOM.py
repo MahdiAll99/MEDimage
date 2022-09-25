b6145d593360f9fc31e409c05ef146cf87c3c691
mahdiall99 <mehdiwinneri01@gmail.com>
2022-09-25 14:23:42 -0400

    Added combine slices as method in the ProcessDICOM class

--- MEDimage/wrangling/ProcessDICOM.py
+++ MEDimage/wrangling/ProcessDICOM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import warnings
 from typing import List, Union
 
@@ -76,6 +77,235 @@ class ProcessDICOM():
         
         return orientation
 
+    def __merge_slice_pixel_arrays(self, slice_datasets):
+        first_dataset = slice_datasets[0]
+        num_rows = first_dataset.Rows
+        num_columns = first_dataset.Columns
+        num_slices = len(slice_datasets)
+
+        sorted_slice_datasets = self.__sort_by_slice_spacing(slice_datasets)
+
+        if any(self.__requires_rescaling(d) for d in sorted_slice_datasets):
+            voxels = np.empty(
+                (num_columns, num_rows, num_slices), dtype=np.float32)
+            for k, dataset in enumerate(sorted_slice_datasets):
+                slope = float(getattr(dataset, 'RescaleSlope', 1))
+                intercept = float(getattr(dataset, 'RescaleIntercept', 0))
+                voxels[:, :, k] = dataset.pixel_array.T.astype(
+                    np.float32)*slope + intercept
+        else:
+            dtype = first_dataset.pixel_array.dtype
+            voxels = np.empty((num_columns, num_rows, num_slices), dtype=dtype)
+            for k, dataset in enumerate(sorted_slice_datasets):
+                voxels[:, :, k] = dataset.pixel_array.T
+
+        return voxels
+
+    def __requires_rescaling(self, dataset):
+        return hasattr(dataset, 'RescaleSlope') or hasattr(dataset, 'RescaleIntercept')
+
+    def __ijk_to_patient_xyz_transform_matrix(self, slice_datasets):
+        first_dataset = self.__sort_by_slice_spacing(slice_datasets)[0]
+        image_orientation = first_dataset.ImageOrientationPatient
+        row_cosine, column_cosine, slice_cosine = self.__extract_cosines(
+            image_orientation)
+
+        row_spacing, column_spacing = first_dataset.PixelSpacing
+        slice_spacing = self.__slice_spacing(slice_datasets)
+
+        transform = np.identity(4, dtype=np.float32)
+        rotation = np.identity(3, dtype=np.float32)
+        scaling = np.identity(3, dtype=np.float32)
+
+        transform[:3, 0] = row_cosine*column_spacing
+        transform[:3, 1] = column_cosine*row_spacing
+        transform[:3, 2] = slice_cosine*slice_spacing
+
+        transform[:3, 3] = first_dataset.ImagePositionPatient
+
+        rotation[:3, 0] = row_cosine
+        rotation[:3, 1] = column_cosine
+        rotation[:3, 2] = slice_cosine
+
+        rotation = np.transpose(rotation)
+
+        scaling[0, 0] = column_spacing
+        scaling[1, 1] = row_spacing
+        scaling[2, 2] = slice_spacing
+
+        return transform, rotation, scaling
+
+    def __validate_slices_form_uniform_grid(self, slice_datasets):
+        """
+        Perform various data checks to ensure that the list of slices form a
+        evenly-spaced grid of data.
+        Some of these checks are probably not required if the data follows the
+        DICOM specification, however it seems pertinent to check anyway.
+        """
+        invariant_properties = [
+            'Modality',
+            'SOPClassUID',
+            'SeriesInstanceUID',
+            'Rows',
+            'Columns',
+            'ImageOrientationPatient',
+            'PixelSpacing',
+            'PixelRepresentation',
+            'BitsAllocated',
+            'BitsStored',
+            'HighBit',
+        ]
+
+        for property_name in invariant_properties:
+            self.__slice_attribute_equal(slice_datasets, property_name)
+
+        self.__validate_image_orientation(slice_datasets[0].ImageOrientationPatient)
+
+        slice_positions = self.__slice_positions(slice_datasets)
+        self.__check_for_missing_slices(slice_positions)
+
+    def __validate_image_orientation(self, image_orientation):
+        """
+        Ensure that the image orientation is supported
+        - The direction cosines have magnitudes of 1 (just in case)
+        - The direction cosines are perpendicular
+        """
+
+        row_cosine, column_cosine, slice_cosine = self.__extract_cosines(
+            image_orientation)
+
+        if not self.__almost_zero(np.dot(row_cosine, column_cosine), 1e-4):
+            raise ValueError(
+                "Non-orthogonal direction cosines: {}, {}".format(row_cosine, column_cosine))
+        elif not self.__almost_zero(np.dot(row_cosine, column_cosine), 1e-8):
+            warnings.warn("Direction cosines aren't quite orthogonal: {}, {}".format(
+                row_cosine, column_cosine))
+
+        if not self.__almost_one(np.linalg.norm(row_cosine), 1e-4):
+            raise ValueError(
+                "The row direction cosine's magnitude is not 1: {}".format(row_cosine))
+        elif not self.__almost_one(np.linalg.norm(row_cosine), 1e-8):
+            warnings.warn(
+                "The row direction cosine's magnitude is not quite 1: {}".format(row_cosine))
+
+        if not self.__almost_one(np.linalg.norm(column_cosine), 1e-4):
+            raise ValueError(
+                "The column direction cosine's magnitude is not 1: {}".format(column_cosine))
+        elif not self.__almost_one(np.linalg.norm(column_cosine), 1e-8):
+            warnings.warn(
+                "The column direction cosine's magnitude is not quite 1: {}".format(column_cosine))
+        sys.stderr.flush()
+
+    def __is_close(self, a, b, rel_tol=1e-9, abs_tol=0.0):
+        return abs(a-b) <= max(rel_tol*max(abs(a), abs(b)), abs_tol)
+
+    def __almost_zero(self, value, abs_tol):
+        return self.__is_close(value, 0.0, abs_tol=abs_tol)
+
+    def __almost_one(self, value, abs_tol):
+        return self.__is_close(value, 1.0, abs_tol=abs_tol)
+
+    def __extract_cosines(self, image_orientation):
+        row_cosine = np.array(image_orientation[:3])
+        column_cosine = np.array(image_orientation[3:])
+        slice_cosine = np.cross(row_cosine, column_cosine)
+        return row_cosine, column_cosine, slice_cosine
+
+    def __slice_attribute_equal(self, slice_datasets, property_name):
+        initial_value = getattr(slice_datasets[0], property_name, None)
+        for slice_idx, dataset in enumerate(slice_datasets[1:]):
+            value = getattr(dataset, property_name, None)
+            if value != initial_value:
+                msg = f'Slice {slice_idx+1} have different value for {property_name}: {value} != {initial_value}'
+                warnings.warn(msg)
+
+    def __slice_positions(self, slice_datasets):
+        image_orientation = slice_datasets[0].ImageOrientationPatient
+        row_cosine, column_cosine, slice_cosine = self.__extract_cosines(
+            image_orientation)
+        return [np.dot(slice_cosine, d.ImagePositionPatient) for d in slice_datasets]
+
+    def __check_for_missing_slices(self, slice_positions):
+        slice_positions_diffs = np.diff(sorted(slice_positions))
+        if not np.allclose(slice_positions_diffs, slice_positions_diffs[0], atol=0, rtol=1e-5):
+            msg = "The slice spacing is non-uniform. Slice spacings:\n{}"
+            warnings.warn(msg.format(slice_positions_diffs))
+            sys.stderr.flush()
+        if not np.allclose(slice_positions_diffs, slice_positions_diffs[0], atol=0, rtol=1e-1):
+            raise ValueError('The slice spacing is non-uniform. It appears there are extra slices from another scan')
+
+    def __slice_spacing(self, slice_datasets):
+        if len(slice_datasets) > 1:
+            slice_positions = self.__slice_positions(slice_datasets)
+            slice_positions_diffs = np.diff(sorted(slice_positions))
+            return np.mean(slice_positions_diffs)
+
+        return 0.0
+
+    def __sort_by_slice_spacing(self, slice_datasets):
+        slice_spacing = self.__slice_positions(slice_datasets)
+        return [d for (s, d) in sorted(zip(slice_spacing, slice_datasets))]
+
+    def combine_slices(self, slice_datasets: List[pydicom.dataset.FileDataset]) -> List[np.ndarray]:
+        """
+        Given a list of pydicom datasets for an image series, stitch them together into a
+        three-dimensional numpy array of iamging data. Also calculate a 4x4 affine transformation
+        matrix that converts the ijk-pixel-indices into the xyz-coordinates in the
+        DICOM patient's coordinate system and 4x4 rotation and scaling matrix.
+        If any of the DICOM images contain either the
+        `Rescale Slope <https://dicom.innolitics.com/ciods/ct-image/ct-image/00281053>`__ or the
+        `Rescale Intercept <https://dicom.innolitics.com/ciods/ct-image/ct-image/00281052>`__
+        attributes they will be applied to each slice individually.
+        This function requires that the datasets:
+
+        - Be in same series (have the same
+        `Series Instance UID <https://dicom.innolitics.com/ciods/ct-image/general-series/0020000e>`__,
+        `Modality <https://dicom.innolitics.com/ciods/ct-image/general-series/00080060>`__,
+        and `SOP Class UID <https://dicom.innolitics.com/ciods/ct-image/sop-common/00080016>`__).
+        - The binary storage of each slice must be the same (have the same
+        `Bits Allocated <https://dicom.innolitics.com/ciods/ct-image/image-pixel/00280100>`__,
+        `Bits Stored <https://dicom.innolitics.com/ciods/ct-image/image-pixel/00280101>`__,
+        `High Bit <https://dicom.innolitics.com/ciods/ct-image/image-pixel/00280102>`__, and
+        `Pixel Representation <https://dicom.innolitics.com/ciods/ct-image/image-pixel/00280103>`__).
+        - The image slice must approximately form a grid. This means there can not
+        be any missing internal slices (missing slices on the ends of the dataset
+        are not detected).
+        - It also means that  each slice must have the same
+        `Rows <https://dicom.innolitics.com/ciods/ct-image/image-pixel/00280010>`__,
+        `Columns <https://dicom.innolitics.com/ciods/ct-image/image-pixel/00280011>`__,
+        `Pixel Spacing <https://dicom.innolitics.com/ciods/ct-image/image-plane/00280030>`__, and
+        `Image Orientation (Patient) <https://dicom.innolitics.com/ciods/ct-image/image-plane/00200037>`__
+        attribute values.
+        - The direction cosines derived from the
+        `Image Orientation (Patient) <https://dicom.innolitics.com/ciods/ct-image/image-plane/00200037>`__
+        attribute must, within 1e-4, have a magnitude of 1.  The cosines must
+        also be approximately perpendicular (their dot-product must be within
+        1e-4 of 0).  Warnings are displayed if any of theseapproximations are
+        below 1e-8, however, since we have seen real datasets with values up to
+        1e-4, we let them pass.
+        - The `Image Position (Patient) <https://dicom.innolitics.com/ciods/ct-image/image-plane/00200032>`__
+        values must approximately form a line.
+        
+        If any of these conditions are not met, a `dicom_numpy.DicomImportException` is raised.
+
+        Args:
+            slice_datasets (List[pydicom.dataset.FileDataset]): List of dicom headers.
+        Returns:
+            List[numpy.ndarray]: List of numpy arrays containing the data extracted the dicom files
+            (voxels, translation, rotation and scaling matrix).
+        """
+
+        if not slice_datasets:
+            raise ValueError("Must provide at least one DICOM dataset")
+
+        self.__validate_slices_form_uniform_grid(slice_datasets)
+
+        voxels = self.__merge_slice_pixel_arrays(slice_datasets)
+        transform, rotation, scaling = self.__ijk_to_patient_xyz_transform_matrix(
+            slice_datasets)
+
+        return voxels, transform, rotation, scaling
+
     @ray.remote
     def process_files(self) -> MEDimage:
         """
@@ -88,8 +318,6 @@ class ProcessDICOM():
         Returns:
             MEDimg (MEDimage): Instance of a MEDimage class.
         """
-        # Since we created a worker, we need to add code path to the system
-        from .combine_slices import combine_slices
 
         # PARTIAL PARSING OF ARGUMENTS
         if self.path_images is None:
@@ -111,7 +339,7 @@ class ProcessDICOM():
             # missing slices and oblique restrictions apply see the reference:
             # https://dicom-numpy.readthedocs.io/en/latest/index.html#dicom_numpy.combine_slices
             try:
-                voxel_ndarray, ijk_to_xyz, rotation_m, scaling_m = combine_slices(dicom_hi)
+                voxel_ndarray, ijk_to_xyz, rotation_m, scaling_m = self.combine_slices(dicom_hi)
             except ValueError as e:
                 raise ValueError(f'Invalid DICOM data for combine_slices(). Error: {e}')
 
