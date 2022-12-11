e03b9981d6c3284cb136da9239c6f03a89d7d993
mahdiall99 <mehdiwinneri01@gmail.com>
2022-12-11 10:26:29 -0500

    Updated search for dicom files using pathlib

--- scripts/process_dataset.py
+++ scripts/process_dataset.py
@@ -22,15 +22,16 @@ def main(path_dataset: Union[str, Path]) -> None:
         folders_imaging_summary = [f for f in folder_patient_id.iterdir() if f.is_dir()]
         for folder_imaging_summary in folders_imaging_summary:
             # get all dicom files
-            dicom_files = os.listdir(folder_imaging_summary)
+            dicom_files = Path(folder_imaging_summary).glob('**/*')
+            dicom_files = [f for f in dicom_files if f.is_file()]
             for file in dicom_files:
-                if pydicom.misc.is_dicom(folder_imaging_summary / file):
+                if pydicom.misc.is_dicom(file):
                     # update the dicom header fields
-                    dicom_header = pydicom.dcmread(str(folder_imaging_summary / file))
+                    dicom_header = pydicom.dcmread(str(file))
                     dicom_header.PatientID = os.path.basename(folder_patient_id)
                     if 'SeriesDescription' in dicom_header:
                         dicom_header.SeriesDescription = os.path.basename(folder_imaging_summary)
-                    dicom_header.save_as(str(folder_imaging_summary / file))
+                    dicom_header.save_as(str(file))
 
 if __name__ == "__main__":
     # setting up arguments:
